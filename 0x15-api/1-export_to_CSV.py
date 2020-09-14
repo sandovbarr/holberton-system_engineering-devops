53f614b3c5d77aa18c735d6fdc886b113659ec22
Fernando <sandovbarr@gmail.com>
2020-09-14 15:49:51 -0700

    [ADD] export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+    Records all tasks that are owned by this employee
+    Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    File name must be: USER_ID.csv
+'''
+import csv
+import requests
+from sys import argv
+
+
+try:
+    int(argv[1])
+    id_user = argv[1]
+    user_data = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(id_user))
+    user_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos',
+        params={
+            'userId': id_user})
+
+    usr_nme = user_data.json()['name']
+    user_tasks_json = user_tasks.json()
+
+    with open('USER_ID.csv', 'w') as file:
+        writer = csv.writer(
+            file,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_ALL)
+        for task in user_tasks_json:
+            status = task['completed']
+            task_tit = task['title']
+            writer.writerow([id_user, usr_nme, status, task_tit])
+
+except Exception:
+    print('Not a valid argument')
